c48f6cbe4132a250ca9ca2f8fe4f5c6697cdc627
Ilya Razenshteyn <ilyaraz@gmail.com>
2012-12-28 22:11:44 -0500

    no newline in password

--- gen1.py
+++ gen1.py
@@ -10,11 +10,16 @@ clue = sys.argv[1]
 length = int(sys.argv[2])
 
 def shellquote(s):
-    return "'" + s.replace("'", "'\\''") + "'"
+    return "'" + s.replace("'", "'\\''") + "\c'"
 
 symb = [chr(x) for x in xrange(33, 127)]
 
 password = getpass.getpass()
+
+h = hashlib.new('sha512')
+h.update(password)
+h.update('')
+print h.hexdigest()[:5]
 
 h = hashlib.new('sha512')
 h.update(password)
