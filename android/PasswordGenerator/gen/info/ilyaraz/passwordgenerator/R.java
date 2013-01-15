68f2e700bffc3190810a7055258e030df6359885
Ilya Razenshteyn <ilyaraz@gmail.com>
2013-01-15 15:37:49 -0500

    an ugly version of the main screen

--- android/PasswordGenerator/gen/info/ilyaraz/passwordgenerator/R.java
+++ android/PasswordGenerator/gen/info/ilyaraz/passwordgenerator/R.java
@@ -11,10 +11,18 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int check=0x7f020000;
+        public static final int cross=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int add_clue_button=0x7f070003;
+        public static final int clue=0x7f070002;
+        public static final int copy_button=0x7f070005;
+        public static final int is_master_password_correct=0x7f070001;
+        public static final int master_password=0x7f070000;
+        public static final int menu_settings=0x7f070006;
+        public static final int password=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -23,8 +31,8 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f040001;
+        public static final int master_password=0x7f040000;
         public static final int menu_settings=0x7f040002;
     }
     public static final class style {
